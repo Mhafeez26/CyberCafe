04162d7282b0ed7adb5975925ae58052d1e7eee1
Mhafeez26 <mhafeezhaneef07@gmail.com>
2026-06-07 11:36:06 +0500

    Add sqlexcetption handler function

--- src/util/ExceptionHandler.java
+++ src/util/ExceptionHandler.java
@@ -1,4 +1,17 @@
 package util;
 
-public class ExceptionHandler {
+import javax.swing.JOptionPane;
+import java.awt.Component;
+import java.sql.SQLException;
+
+
+public class ExceptionHandler{
+
+
+    public static void handleSQLException(SQLException e,Component parent){
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(parent,
+                "Database Error:\n"+e.getMessage(),
+                "DB Error",JOptionPane.ERROR_MESSAGE);
+   }
 }
